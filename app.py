4cf1689933b58294dca434b1a5a1e5ff1852174b
hjghiuiunehfuhjk4wb <214203@clvsc.tyc.edu.tw>
2025-02-13 14:06:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-# 創建 Flask 應用
 app = Flask(__name__)
 
-# 設定首頁路由
-@app.route('/')
-def home():
-    return 'Hello 你好'
+# 在這裡填入你的 LINE Bot 的 Channel Access Token 和 Channel Secret
+LINE_CHANNEL_ACCESS_TOKEN = '你的 Channel Access Token'
+LINE_CHANNEL_SECRET = '你的 Channel Secret'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+# 用來接收 webhook 事件的路由
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        # 驗證並處理 webhook 事件
+        line_handler.handle(body, signature)
+    except Exception as e:
+        abort(400)
+
+    return 'OK'
+
+# 設定處理訊息事件的邏輯
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # 回應使用者發送的文字訊息
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=f"你發送的訊息是: {event.message.text}")
+    )
+
+if __name__ == "__main__":
+    app.run(port=5000)
